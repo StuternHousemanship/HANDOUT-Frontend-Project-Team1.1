662c1333bd40bd9e763793ab79824038d1227721
ebiapollos <doyinapollos@gmail.com>
2022-10-18 12:01:19 +0100

    Apollos: HDT-1 Onboarding Features v1

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+    return ( <
+        div className = "app" >
+        <
+        header className = "App-header" >
+        <
+        img src = { logo }
+        className = "App-logo"
+        alt = "logo" / >
+        <
+        p > A platform
+        for unwanted personal items < /p> <
+        p >
+        This platform allows people to give out useful but unwanted items to others who may need them.. <
+        /p> <
+        /header> <
+        /div>
+    );
+}
+
+export default App;
