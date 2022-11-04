075ced547fa960a5a14e8042adb0cd930e704c3e
ebiapollos <doyinapollos@gmail.com>
2022-11-04 21:29:05 +0100

    Apollos T3: HDT-1 Onboarding Features V20

--- src/routers.jsx
+++ src/routers.jsx
@@ -6,7 +6,6 @@ import { ReactComponent as LoadingIcon } from "./assets/svg/loading-icon.svg";
 import Home from "./pages/Home";
 import SignUp from "./pages/SignUp";
 import LogIn from "./pages/LogIn";
-import Logout from "./pages/Logout";
 import VerifyEmail from "./pages/VerifyEmail";
 import Dashboard from "./pages/Dashboard";
 
@@ -25,7 +24,6 @@ function Routers() {
           <Route path={NonAuthRoutes.Home} element={<Home />} />
           <Route path={NonAuthRoutes.SignUp} element={<SignUp />} />
           <Route path={NonAuthRoutes.LogIn} element={<LogIn />} />
-          <Route path={NonAuthRoutes.Logout} element={<Logout />} />
           <Route path={NonAuthRoutes.VerifyEmail} element={<VerifyEmail />} />
           <Route path={AuthRoutes.Dashboard} element={<Dashboard />} />
         </Routes>
