bccf386c5f593224c43a3fe425a0db5347e7ee73
ebiapollos <doyinapollos@gmail.com>
2022-11-14 19:53:36 +0100

    Apollos T3: HDT-1 Onboarding Features V33

--- src/pages/VerifyEmail.jsx
+++ src/pages/VerifyEmail.jsx
@@ -1,28 +1,38 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import logo from "../assets/svg/desktop.svg";
 
 function VerifyEmail() {
   return (
     <div className="flex flex-col justify-center items-center w-full h-[100vh]">
       {/* laptop and ipad view */}
-      <div className="xs: heddin flex flex-col justify-center items-center w-full h-[100vh] bg-[#E5E5E5]">
-        <div className="xs:hidden md:inline-flex lg:inline-flex flex flex-col justify-center items-center w-full h-h-[100vh]">
-          <div className="w-largeScreenWidth h-largeScreenHeight bg-white">
-            <div className="ml-20">
-              <div className="w-80 h-11 mt-12 font-bold leading-10 text-4xl text-buttonColor">
+      <div className="xs: heddin flex flex-col justify-center items-center w-full h-[100vh] bg-white">
+        <div className="xs:hidden md:inline-flex lg:inline-flex w-full h-[80px] bg-white border-b-[3px] border-[#F1F7F7] ">
+          <img
+            className="w-[157px] h-[44] ml-[120px] my-[26px]"
+            src={logo}
+            alt=""
+          />
+        </div>
+
+        <div className="xs:hidden md:inline-flex lg:inline-flex flex flex-col justify-center items-center w-full h-[100vh]">
+          <div className="w-[640px] h-[500px] bg-[#F1F7F7]">
+            <div className="flex flex-col justify-center items-center">
+              <div className="w-80 h-11 mt-12 font-[700] leading-40 text-[32px] font-Raleway text-[#2F2F2A]">
                 Verify your email
               </div>
 
-              <div className="w-96 h-10 mt-14 leading-5 text-black font-normal">
-                Kindly go to your email inbox or spam folder to confirm and
-                verify your email address.
+              <hr className="w-[540px] border-1 border-[#969696] mt-[24px] " />
+
+              <div className="w-96 h-10 mt-14 font-[500] leading-[24px] text-[#4B4B4B] font-Raleway text-center text-[16px]">
+                Glad to have you join the community. Click the button below to
+                complete setting up your account
               </div>
 
               <button
                 type="button"
-                className="w-largeScreenHeight h-14 rounded bg-buttonColor mt-20 text-white"
+                className="w-[502px] h-[59px] font-[700]  rounded bg-[#077369]  mt-20 text-[#FEFEFE] text-[20px]"
               >
-                Okay
+                Continue
               </button>
             </div>
           </div>
@@ -31,21 +41,28 @@ function VerifyEmail() {
 
       {/* Mobile view */}
       <div className="md:hidden lg:hidden flex flex-col justify-center items-center w-full h-[100vh]">
-        <div className="flex flex-col justify-center items-center w-[428px] h-[926px] mt-24 my-10 bg-white">
-          <div className="w-full h-subWidth">
-            <div className="ml-14">
-              <div className="w-56 h-8 mt-9 font-bold leading-8 text-[27.04px] text-buttonColor">
+        <div className="md:hidden lg:hidden w-full h-[80px] bg-white border-b-[2px] border-[#F1F7F7] ">
+          <img
+            className="w-[104px] h-[26] ml-[20px] my-[23px]"
+            src={logo}
+            alt=""
+          />
+        </div>
+        <div className="flex flex-col justify-center items-center w-[428px] h-[926px]  my-10 bg-white">
+          <div className="w-full h-subWidth ">
+            <div className="flex flex-col justify-center items-center">
+              <div className="w-56 h-8  font-[700] text-[24px] leading-[34px] font-Raleway text-[#191919]">
                 Verify your email
               </div>
-              <div className="w-80 h-10 mt-11">
-                Kindly go to your email inbox or spam folder to confirm and
-                verify your email address.
+              <div className="w-80 h-10 mt-11 text-[13px] leading-[24px] text-center font-Raleway text-[#191919] font-[400] ">
+                Glad to have you join the community. Click the button to
+                complete setting up your account
               </div>
               <button
                 type="button"
-                className="w-80 h-11 bg-buttonColor mt-14 text-white"
+                className="w-[388px] h-[53px] bg-[#077369] font-[700] mt-14 text-[#FEFEFE] text-[16px] tracking-wide font-Raleway "
               >
-                <Link to="/login"> Okay </Link>
+                Verify
               </button>
             </div>
           </div>
