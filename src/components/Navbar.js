6f714e3a256a133466617711070d1bc79b3d535f
Olorunsolavictor <victorolorunsola@rocketmail.com>
2022-11-15 04:03:22 -0800

    Victor T2: HDT-1 Onboarding Features V36

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ import logo from "../assets/svg/desktop.svg";
 function Navbar() {
   return (
     <div className="">
-      <nav className="bg-white flex items-center shadow-md w-full h-[10%] z-[100] fixed top-0 left-0  ">
+      <nav className="bg-white flex items-center shadow-3xl w-full h-[10%] z-[100] absolute top-0 left-0  ">
         <img src={logo} className=" ml-[128px] h-[30px] " alt="Handout" />
       </nav>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,32 +1,35 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/**/*.{js,jsx,ts,tsx}"],
-    theme: {
-        screens: {
-            xs: "300px",
-            sm: "429px",
-            md: "768px",
-            lg: "1024px",
-            xl: "1280px",
-            "2xl": "1536px",
-        },
-        extend: {
-            fontFamily: {
-                Inter: ["Inter"],
-                Raleway: ["Raleway"],
-            },
-            spacing: {
-                mobileWidth: "26.75rem",
-                mobileHeight: "57.875rem",
-                subWidth: "20.563rem",
-                biggerScreenWidth: "56.25rem",
-                largeScreenWidth: "38rem",
-                largeScreenHeight: "27.375rem",
-            },
-            colors: {
-                buttonColor: "#424242",
-            },
-        },
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    screens: {
+      xs: "300px",
+      sm: "429px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
     },
-    plugins: [],
+    extend: {
+      fontFamily: {
+        Inter: ["Inter"],
+        Raleway: ["Raleway"],
+      },
+      spacing: {
+        mobileWidth: "26.75rem",
+        mobileHeight: "57.875rem",
+        subWidth: "20.563rem",
+        biggerScreenWidth: "56.25rem",
+        largeScreenWidth: "38rem",
+        largeScreenHeight: "27.375rem",
+      },
+      colors: {
+        buttonColor: "#424242",
+      },
+      boxShadow: {
+        "3xl": "0px 4px 4px rgba(0, 0, 0, 0.1)",
+      },
+    },
+  },
+  plugins: [],
 };
